ef3c4a8b9f59d6cfa07b062b1694ea4b1f13158d
Nils <52067471+nils-n@users.noreply.github.com>
2024-01-10 16:34:54 +0000

    add Product, Season, Category model

--- city_cycling/settings.py
+++ city_cycling/settings.py
@@ -20,7 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-&zvrr2ep%(e3nu0k+@4%&0^2tmvcbw^zt+&6q449to7g6somqv"
+SECRET_KEY = (
+    "django-insecure-&zvrr2ep%(e3nu0k+@4%&0^2tmvcbw^zt+&6q449to7g6somqv"
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -41,6 +43,7 @@ INSTALLED_APPS = [
     "crispy_forms",
     "crispy_tailwind",
     "accounts",  # new
+    "products",
 ]
 
 MIDDLEWARE = [
--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from products.models import Product, Category, Season
 # Register your models here.
+
+admin.site.register(Product)
+admin.site.register(Category)
+admin.site.register(Season)
