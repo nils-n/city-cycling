565c727180a46f973147a3151a2dfb138072e41e
Nils <52067471+nils-n@users.noreply.github.com>
2024-01-11 09:22:53 +0000

    add category buttons to products page

--- products/views.py
+++ products/views.py
@@ -1,15 +1,17 @@
 from django.shortcuts import render
-from products.models import Product
+from products.models import Product, Category
 
 
 def all_products(request):
     """view to display all products in the shop"""
     products = Product.objects.all()
+    categories = Category.objects.all()
 
     return render(
         request,
         "products/products.html",
         context={
             "products": products,
+            "categories": categories,
         },
     )
