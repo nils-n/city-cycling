8396b181da202d091d48b30d7a83a10d139ca10e
Nils <52067471+nils-n@users.noreply.github.com>
2024-01-14 19:39:43 +0000

    add functionality to add products to bag

--- bag/urls.py
+++ bag/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from bag.views import view_bag
+from bag.views import view_bag, add_to_bag
 
 urlpatterns = [
     path("", view_bag, name="view_bag"),
+    path("add/<item_id>", add_to_bag, name="add_to_bag"),
 ]
--- bag/views.py
+++ bag/views.py
@@ -1,7 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from icecream import ic
 
 
 def view_bag(request):
     """view content of shopping bag"""
 
     return render(request, "bag/bag.html")
+
+
+def add_to_bag(request, item_id):
+    """add a product to the shopping bag"""
+
+    quantity = int(request.POST.get("quantity"))
+    redirect_url = request.POST.get("redirect_url")
+
+    # get bag from session if it exists
+    bag = request.session.get("bag", {})
+
+    if item_id in list(bag.keys()):
+        bag[item_id] += quantity
+    else:
+        bag[item_id] = quantity
+
+    request.session["bag"] = bag
+
+    # for testing, log out the content the console
+    ic(bag)
+
+    return redirect(redirect_url)
