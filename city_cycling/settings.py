24327cae2a6e736a6d94df240654f585257a7398
Nils <52067471+nils-n@users.noreply.github.com>
2024-01-14 19:08:55 +0000

    add shoppingbag context processor

--- city_cycling/settings.py
+++ city_cycling/settings.py
@@ -81,6 +81,7 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
+                "bag.contexts.bag_contents",
             ],
         },
     },
@@ -199,3 +200,7 @@ CRISPY_ALLOWED_TEMPLATE_PACKS = "tailwind"
 
 
 CRISPY_TEMPLATE_PACK = "tailwind"
+
+# based on CI Boutique Ado example
+FREE_DELIVERY_THRESHOLD = 50
+STANDARD_DELIVERY_PERCENTAGE = 10
