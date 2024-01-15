ecdddc27d5e0ee622b24df098d4a820f3bac46c4
Nils <52067471+nils-n@users.noreply.github.com>
2024-01-15 15:21:35 +0000

    enable addding products with different sizes to bag

--- bag/views.py
+++ bag/views.py
@@ -12,14 +12,28 @@ def add_to_bag(request, item_id):
 
     quantity = int(request.POST.get("quantity"))
     redirect_url = request.POST.get("redirect_url")
+    size = None
+
+    if "product_size" in request.POST:
+        size = request.POST.get("product_size")
 
     # get bag from session if it exists
     bag = request.session.get("bag", {})
 
-    if item_id in list(bag.keys()):
-        bag[item_id] += quantity
+    # allow for multiple size of a product that has sizes
+    if size:
+        if item_id in list(bag.keys()):
+            if size in bag[item_id]["items_by_size"].keys():
+                bag[item_id]["items_by_size"][size] += quantity
+            else:
+                bag[item_id]["items_by_size"][size] = quantity
+        else:
+            bag[item_id] = {"items_by_size": {size: quantity}}
     else:
-        bag[item_id] = quantity
+        if item_id in list(bag.keys()):
+            bag[item_id] += quantity
+        else:
+            bag[item_id] = quantity
 
     request.session["bag"] = bag
 
