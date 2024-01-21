3a45497e0c40a1f309b20979368e5bd08abce083
Nils <52067471+nils-n@users.noreply.github.com>
2024-01-21 19:07:30 +0000

    deployment commit

--- city_cycling/settings.py
+++ city_cycling/settings.py
@@ -153,7 +153,7 @@ STATIC_URL = "/static/"
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
-# STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 
 MEDIA_URL = "/media/"
