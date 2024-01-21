0dcf14be04f812e4520ae6ed1dab0e4ea8fb091e
Nils <52067471+nils-n@users.noreply.github.com>
2024-01-21 11:48:56 +0000

    add functionality to edit products as admin

--- products/views.py
+++ products/views.py
@@ -83,3 +83,36 @@ def add_product(request):
     context = {"form": form}
 
     return render(request, template, context)
+
+
+def edit_product(request, product_id):
+    """edit a product in the store"""
+    product = get_object_or_404(Product, pk=product_id)
+
+    if request.POST:
+        form = ProductForm(request.POST, request.FILES, instance=product)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Product successfully edited.")
+            return redirect(
+                reverse(
+                    "product_detail",
+                    args=[product.id],
+                )
+            )
+        else:
+            form = ProductForm(instance=product)
+            messages.error(
+                request,
+                "Faild to update Product. Have you entered the form correctly?",
+            )
+
+    else:
+        form = ProductForm(instance=product)
+        messages.info(request, f"You are editing {product.name}")
+
+    template = "products/edit_product.html"
+
+    context = {"form": form, "product": product}
+
+    return render(request, template, context)
