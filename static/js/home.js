ebf0e818398c13b63d073006b559a99d315aae42
Nils <52067471+nils-n@users.noreply.github.com>
2024-01-24 14:45:54 +0000

    improve responsive layout of newsletter banner

--- static/js/home.js
+++ static/js/home.js
@@ -1,12 +1,23 @@
 const modalBtn = document.getElementById("newsletter-btn");
-
+const newsletterInputEl = document.getElementById("mc-embedded-subscribe");
+const newsletterMailInputEl = document.getElementById("mce-EMAIL");
 console.log("home js loaded");
 
 // // make newsletter modal appear after.5
-// document.addEventListener("DOMContentLoaded", (e) => {
-//   console.log("document loaded");
-//   setTimeout(() => {
-//     modalBtn.click();
-//     console.log("loadin modal");
-//   }, 3000);
-// });
+document.addEventListener("DOMContentLoaded", (e) => {
+  console.log("document loaded");
+  console.log("removing margins from MC form");
+  console.log(newsletterInputEl.style.marginTop);
+
+  newsletterInputEl.style.marginBottom = "0.5em";
+  newsletterInputEl.style.marginTop = "0.5em";
+
+  newsletterMailInputEl.style.marginBottom = "0.5em";
+  newsletterMailInputEl.style.marginTop = "0.5em";
+
+  console.log(newsletterInputEl.style.marginTop);
+  //   setTimeout(() => {
+  //     modalBtn.click();
+  //     console.log("loadin modal");
+  //   }, 3000);
+});
