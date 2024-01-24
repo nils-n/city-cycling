25e50df2e25e2afe074b7f089d38a74b78c73f43
Nils <52067471+nils-n@users.noreply.github.com>
2024-01-24 12:48:22 +0000

    work on styling of newsletter signup banner

---
+++ static/js/home.js
@@ -0,0 +1,12 @@
+const modalBtn = document.getElementById("newsletter-btn");
+
+console.log("home js loaded");
+
+// // make newsletter modal appear after.5
+// document.addEventListener("DOMContentLoaded", (e) => {
+//   console.log("document loaded");
+//   setTimeout(() => {
+//     modalBtn.click();
+//     console.log("loadin modal");
+//   }, 3000);
+// });
