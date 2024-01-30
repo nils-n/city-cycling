a5466fac629650be5d17e81eb3c7d87135ae03d5
Nils <52067471+nils-n@users.noreply.github.com>
2024-01-30 03:24:09 +0000

    add setting for verification emails

--- city_cycling/settings.py
+++ city_cycling/settings.py
@@ -239,3 +239,16 @@ DEFAULT_FROM_EMAIL = "glasgowcitycycling@example.com"
 COMPRESS_ENABLED = os.environ.get("COMPRESS_ENABLED", False)
 COMPRESS_URL = "/static/"
 # COMPRESS_ROOT = STATIC_ROOT
+
+
+if DEBUG:
+    EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+    DEFAULT_FROM_EMAIL = "citycycling@example.com"
+else:
+    EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+    EMAIL_USE_TLS = True
+    EMAIL_PORT = 587
+    EMAIL_HOST = os.getenv("EMAIL_HOST", "")
+    EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER", "")
+    EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASS", "")
+    DEFAULT_FROM_EMAIL = os.getenv("EMAIL_HOST_USER", "")
